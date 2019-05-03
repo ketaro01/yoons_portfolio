13d17e40dd92396ebaaaf4d416ba0ee9131867d1
ketaro01 <ketaro01@naver.com>
2019-05-03 22:52:00 +0900

    Updates

---
+++ precache-manifest.04e13316e1d566eca95227e54d8ee05f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05503f09bb662b2578d3e85bfbb9e1cc",
+    "url": "/yoons_portfolio/index.html"
+  },
+  {
+    "revision": "121f102a2f9fb611dfe6",
+    "url": "/yoons_portfolio/static/css/2.400ebc83.chunk.css"
+  },
+  {
+    "revision": "4fcd975e56b0dd40e0b8",
+    "url": "/yoons_portfolio/static/css/main.c866f012.chunk.css"
+  },
+  {
+    "revision": "121f102a2f9fb611dfe6",
+    "url": "/yoons_portfolio/static/js/2.cb2aa175.chunk.js"
+  },
+  {
+    "revision": "4fcd975e56b0dd40e0b8",
+    "url": "/yoons_portfolio/static/js/main.815aff52.chunk.js"
+  },
+  {
+    "revision": "2fcc4a0db8e8b444f857",
+    "url": "/yoons_portfolio/static/js/runtime~main.b751ddf1.js"
+  },
+  {
+    "revision": "0a0cff340c7097bbc0e2c93c7897166a",
+    "url": "/yoons_portfolio/static/media/img-bg-sample-parallax-header.0a0cff34.jpg"
+  },
+  {
+    "revision": "254ad5f12c19b427b6513af22e83c03a",
+    "url": "/yoons_portfolio/static/media/wwwww.254ad5f1.png"
+  }
+]);
